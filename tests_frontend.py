ce15a3877aececa4b3193fc32b0fcd47ff7472ba
clement.lesaulnier <lesaulnc@lesaulnc-pc>
2020-07-21 11:58:32 +0200

    test visibilité restreinte prets

--- tests_frontend.py
+++ tests_frontend.py
@@ -14,11 +14,35 @@ ff_profile = selenium.webdriver.FirefoxProfile(profile_directory=ff_profile_dir)
 driver = Firefox(ff_profile, service_log_path="/home/lesaulnc/geckodriver.log")
 
 
+user1 = "user"
+password_user1 = "cyborg22"
+
+# manager of other entity than user1's loan entity
+user2 = "lesaulnc"
+password_user2 = "montAiguille2020"
+
+# name of entity
+entity1 = "ge2lab"
+
 def check_exists_by_selector(selector):
     el = None
     if len(driver.find_elements(By.CSS_SELECTOR,selector)) > 0:
         el = driver.find_element(By.CSS_SELECTOR,selector)
     return el
+
+def check_values_by_selector(value, selector, target):
+    for l in selector:
+        loan = l.find_elements(By.TAG_NAME, target) 
+        if loan[0].text != value:
+            return False
+            break
+    print('all values match' + value)
+    return True
+
+def find_table_selector():
+    selector = driver.find_element(By.CSS_SELECTOR,".row").find_elements(By.CSS_SELECTOR, ".card")[0]
+    table_selector = selector.find_elements(By.CSS_SELECTOR,"table tbody tr")
+    return table_selector
 
 class MaterialLoans:
     server_url = "http://localhost:8000"
@@ -34,9 +58,8 @@ class MaterialLoans:
         except ValueError:
             print("login error")
 
-    def loan(self):
+    def loan(self, target):
         try:    
-            self.login("user", "cyborg22") # set user not manager 
             driver.get(self.server_url+"/#/search")
             time.sleep(1)
             filter_type = driver.find_elements(By.TAG_NAME, 'select')
@@ -50,14 +73,13 @@ class MaterialLoans:
                 entities = e.find_elements(By.TAG_NAME, 'a') 
                 add_material = e.find_elements(By.TAG_NAME, 'button')
                 for entity in entities:
-                    if entity.text == 'ge2lab':
-                        print("ajout")
-                        print(add_material[0].text)
+                    print()
+                    if entity.text == target:
                         add_material[0].click()
                         clicked = True
                 if clicked:
                     break
-
+            time.sleep(4)
             driver.get(self.server_url+"/#/loan")
             time.sleep(2)
             inputs_date =  driver.find_elements(By.CSS_SELECTOR, '[type="date"]')
@@ -87,12 +109,11 @@ class MaterialLoans:
         try:
             tables = driver.find_elements(By.CSS_SELECTOR,"table")
             table_loans = tables[0].find_elements(By.CSS_SELECTOR,"tbody tr")
+            time.sleep(3)
             for entries in table_loans:
                 col = entries.find_elements(By.TAG_NAME, "td")
-                if col[0].text == "user":
-                    assert( col[1].text == status and col[2].text == "2020-07-08" and col[3].text == "2020-07-09")
-                    col[0].click()
-                    break
+                col[0].click()
+                break
             actions = driver.find_elements(By.CSS_SELECTOR,".btn-group.float-right button")
             actions[0].click()
             time.sleep(1)
@@ -116,22 +137,44 @@ class MaterialLoans:
 try:
     
     """
-    test utilisateur d'un emprunt non assignable pour une autre personne
+    test emprunt non assignable pour une autre personne
     """
-  
     material_loans = MaterialLoans()
-    material_loans.loan()
-    time.sleep(3)
+    material_loans.login(user1, password_user1)
+    material_loans.loan(entity1)
+    time.sleep(2)
     driver.get(material_loans.server_url+"/#/entities/1/loans")
     time.sleep(1)
-    MaterialLoans.update_loan(1,"2020-05-08","Demandé")
+    MaterialLoans.update_loan(4,"2020-05-09","Accepté")
     time.sleep(1)
+    status =  driver.find_element(By.CSS_SELECTOR, 'select')
+    assert(status.get_attribute("disabled") == "true")
     # check if user input is present    
     assert(check_exists_by_selector(".form-control.p-0") is None)
+
+    """
+    test accès restreint des prêts aux utilisateur non manager d'une entité
+    """
+
+    driver.get(material_loans.server_url+"/#/entities/1/loans")
+    time.sleep(1)
+    table_selector = find_table_selector()
+    assert(check_values_by_selector(user1,table_selector, "td") is False)
+    time.sleep(1)
+    driver.get(material_loans.server_url+'/auth/logout')
+    time.sleep(1)
+
+    # check user manager of other entity
+    material_loans.login(user2, password_user2)
+    time.sleep(2)
+    driver.get(material_loans.server_url+"/#/entities/1/loans")
+    time.sleep(1)
+    table_selector = find_table_selector()
+    assert(check_values_by_selector(user2,table_selector, "td") is True)
 
 except ValueError as e:
     print(e)
     
 finally:
     print('finally')
-    #driver.quit()
+    driver.quit()
