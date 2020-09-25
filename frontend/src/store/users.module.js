ecd11c276291c6c8b211f223f3df4ee58de5a492
Clement Lesaulnier <clement.lesaulnier@grenoble-inp.fr>
2020-09-25 18:27:04 +0200

    merge users.module

--- frontend/src/store/users.module.js
+++ frontend/src/store/users.module.js
@@ -1,10 +1,8 @@
 import createCrud from "@/common/storeCrudHelper";
 
-
 const users_extra = {
   getters: {},
   mutations: {}
 };
-
 const users = createCrud("users", users_extra);
 export default users;
