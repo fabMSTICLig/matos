88b6b3ba4c720499a8d8b302aaab81b5722e675e
clement.lesaulnier <lesaulnc@lesaulnc-pc>
2020-10-02 12:40:17 +0200

    filtre instances matériels spécifique prêt

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.db import models
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Entity, Affiliation, Tag, SpecificMaterial, GenericMaterial, SpecificMaterialInstance, Loan, LoanGenericItem
-
+from django import forms
 
 class CustomUserAdmin(UserAdmin):
     ...
@@ -30,13 +30,13 @@ class TagAdmin(admin.ModelAdmin):
     pass
 
 
-class SpecificMaterialInstance(admin.TabularInline):
+class SpecificMaterialInstanceAdmin(admin.TabularInline):
     model = SpecificMaterialInstance
 
 @admin.register(SpecificMaterial)
 class SpecificMaterialAdmin(admin.ModelAdmin):
     model = SpecificMaterial
-    inlines = [SpecificMaterialInstance]
+    inlines = [SpecificMaterialInstanceAdmin]
 
 
 
@@ -47,8 +47,26 @@ class GenericMaterialAdmin(admin.ModelAdmin):
 class LoanGenericItemAdmin(admin.TabularInline):
     model = LoanGenericItem
 
+class LoanAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(LoanAdminForm, self).__init__(*args, **kwargs)
+        print(self.fields['specific_materials'])
+        wsm = SpecificMaterial.objects.filter(entity=self.instance.entity_id)
+        spec_instances = []
+
+        for mat in wsm:
+            instances = SpecificMaterialInstance.objects.filter(model=mat.id)
+            print(instances)
+            for instance in instances:
+                spec_instances.append((instance.id, mat.name + " ("+ instance.name +") " ))
+       
+        w=self.fields['specific_materials'].widget
+       
+        w.choices = spec_instances
+
 @admin.register(Loan)
 class LoanAdmin(admin.ModelAdmin):
     model = Loan
     inlines = [LoanGenericItemAdmin]
+    form = LoanAdminForm
     filter_horizontal  = ('specific_materials',)
