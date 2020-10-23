bfeaae0156414600e44a9f365a1a41f450a558f0
Clément <clement.lesaulnier@grenoble-inp.fr>
2020-10-23 11:39:34 +0200

    modification Footer - ajout organismes

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,19 +1,46 @@
 <template>
   <footer>
     <div class="container mt-3 mb-3 border-top border-dark">
-      <a class="logo-font" href="/">
-        Matos
-      </a>
-      |
-      <a class="logo-font" href="/legalnotice">
-        Legal notice
-      </a>
+      <div class="row d-inline-flex">
+        <div class="col col-lg-6">
+          <div class="row mt-5">
+            <div class="col">
+              <a class="logo-font" href="/">
+                Matos
+              </a>
+              |
+              <a class="logo-font" href="/legalnotice">
+                Legal notice
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col col-lg-6 media">
+          <img :src="`${publicPath}logos-inp-uga.jpg`" width="50%" class="mr-4" />
+          <img :src="`${publicPath}nanoelec-logo.jpg`" width="40%" class="mr-3" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <span class="justify"
+            >La plateforme est soutenue par l'IRT Nanoelec au titre du programme
+            d'Investissement d'Avenir financé par l’État français, référence
+            ANR-10- AIRT-05.
+          </span>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: "Footer"
+  name: "Footer",
+  data () {
+  return {
+    publicPath: process.env.BASE_URL
+  }
+}
+
 };
 </script>
