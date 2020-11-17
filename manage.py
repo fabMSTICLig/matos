e6dd9908f4d095cd325596682f500404926fe25c
Clément <clement.lesaulnier@grenoble-inp.fr>
2020-11-17 15:09:14 +0100

    modification nommage module

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangovue.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matos.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
