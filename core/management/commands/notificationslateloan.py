e5f397d0fa37b435d5a460c5fc8197e24a43c5be
Clément <clement.lesaulnier@grenoble-inp.fr>
2020-11-23 20:36:51 +0100

    commande envoie email en retard ou arrivant à échéance

--- core/management/commands/notificationslateloan.py
+++ core/management/commands/notificationslateloan.py
@@ -12,11 +12,21 @@ from core.models import Loan, User, Entity, GenericMaterial, SpecificMaterial, S
 class Command(BaseCommand):
     help = 'Send notification for late return and approching return'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--return', '-r', default=None, dest='return', action='append',
+                            help='add option return')
+
     def handle(self, *args, **options):
-        loans = Loan.objects.filter(return_date=None).filter(due_date__lt=datetime.today())
+        loans = Loan.objects.filter(return_date=None).filter(due_date__gt=datetime.today()).filter(status=3)
+        late = False
+
+        if options['return']:
+            print("opt=return")
+            loans = Loan.objects.filter(return_date__lt=datetime.today()).filter(status=3)
+            late = True
 
         for loan in loans:
-            context = { 'SITE_URL': settings.SITE_URL, 'loan': loan }
+            context = { 'SITE_URL': settings.SITE_URL, 'loan': loan, 'late': late }
             subject = render_to_string(
                 template_name='email/lateloan_subject.txt',
                 context=context
@@ -29,7 +39,7 @@ class Command(BaseCommand):
                 template_name='email/lateloan_message.html',
                 context=context
             )
-            msg = EmailMultiAlternatives(subject, text_content, settings.NOTIFICATION_SENDER, [loan.user.email], reply_to=settings.NOTIFICATION_REPLYTO)
+            msg = EmailMultiAlternatives(subject, text_content, settings.NOTIFICATION_SENDER, [loan.user.email])
             msg.attach_alternative(html_content, "text/html")
             print(msg.message())
             #msg.send()
