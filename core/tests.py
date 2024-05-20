223a2347722c8c09f9f68b650b83b3d5bbd6468b
Germain Lemasson <germain.lemasson@univ-grenoble-alpes.fr>
2024-05-20 11:22:31 +0200

    apply LICENCE

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,19 @@
+"""
+Copyright (C) 2020-2024 LIG Université Grenoble Alpes
+
+
+This file is part of Matos.
+
+Matos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+FacManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with FacManager. If not, see <https://www.gnu.org/licenses/>
+
+@author Germain Lemasson
+@author Clément Lesaulnier
+@author Robin Courault
+"""
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.urls import reverse
--- matos/urls.py
+++ matos/urls.py
@@ -1,4 +1,21 @@
-"""matos URL Configuration
+"""
+Copyright (C) 2020-2024 LIG Université Grenoble Alpes
+
+
+This file is part of Matos.
+
+Matos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+FacManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with FacManager. If not, see <https://www.gnu.org/licenses/>
+
+@author Germain Lemasson
+@author Clément Lesaulnier
+@author Robin Courault
+"""
+"""
+matos URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
