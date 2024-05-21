3d15247cfa736a261fac56b000dc79eb7fb3fa88
Germain Lemasson <germain.lemasson@univ-grenoble-alpes.fr>
2024-05-21 13:29:46 +0200

    tag name is unique

--- core/models.py
+++ core/models.py
@@ -106,7 +106,12 @@ class Tag(models.Model):
     name : str
         Name of the tag
     """
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        return super(User, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.name
 
